5d51336c296345e3b81a6bff5876a4759948d8a8
Nick Jones <nick.jones@arachnid.co.nz>
2020-07-14 20:29:29 +1200

    SVG Images For Teaching.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject nz.co.arachnid.simplex "0.1.0-SNAPSHOT"
   :description "Simplex Example In Clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure            "1.10.1"]
                  [selmer                        "1.12.18"]
                  [instaparse                     "1.4.10"]]
@@ -11,8 +11,8 @@
                                   [org.clojure/java.classpath   "0.3.0"]
                                   [midje                        "1.9.9"]]
 
-                   :plugins      [[lein-ancient "0.6.15"]
-                                  [lein-midje    "3.2.1"]]}
+                   :plugins [[lein-ancient "0.6.15"]
+                             [lein-midje    "3.2.1"]]}
              :uberjar {:aot :all}}
 
   :repl-options {:init-ns nz.co.arachnid.simplex.core
